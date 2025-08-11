d6495749dc4fa588233666672f5bd7142e5d174a
Hrishabh Ayush <hrishabh.ay@gmail.com>
2025-08-11 15:33:25 -0400

    connect wallet button created

--- user-frontend/components/Appbar.tsx
+++ user-frontend/components/Appbar.tsx
@@ -1,43 +1,53 @@
-"use client";
-import { BACKEND_URL } from '@/utils';
-import { useWallet } from '@solana/wallet-adapter-react';
-import {
-    WalletDisconnectButton,
-    WalletMultiButton
-} from '@solana/wallet-adapter-react-ui';
-import { useEffect } from 'react';
-import axios from 'axios';
+'use client';
 
+import '@rainbow-me/rainbowkit/styles.css';
+
+import {
+    getDefaultConfig,
+    ConnectButton,
+    RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+
+import {
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    base,
+    xrplevmTestnet,
+} from 'wagmi/chains';
+
+import {
+    QueryClientProvider,
+    QueryClient,
+} from '@tanstack/react-query';
+
+const config = getDefaultConfig({
+    appName: 'Verge',
+    projectId: 'dd1ed69c86e875fbde469749a73030fd',
+    chains: [mainnet, polygon, optimism, arbitrum, base, xrplevmTestnet],
+    ssr: false,
+});
+
+const queryClient = new QueryClient();
 
 export const Appbar = () => {
-    const { publicKey, signMessage } = useWallet();
+    return (
+        <div className="flex justify-between border-b pb-2 pt-2 bg-white">
+            <div className="text-2xl pl-4 flex justify-center">
+                <img src="/verge.png" alt="Verge" className="h-16 w-auto" />
+            </div>
 
-    async function signAndSend() {
-        if (!publicKey) {
-            return;
-        }
-        
-        const message = new TextEncoder().encode("Sign in to decentralized fiver website");
-        const signature = await signMessage?.(message);
-        const response = await axios.post(`${BACKEND_URL}/v1/user/signin`, {
-            signature,
-            publicKey: publicKey?.toString()
-        });
-
-        // unique token for each new user
-        localStorage.setItem("token", response.data.token);
-    }
-
-    useEffect(() => {
-        signAndSend();
-    }, [publicKey])
-
-    return <div className="flex justify-between border-b pb-2 pt-2 bg-white">
-        <div className="text-2xl pl-4 flex justify-center">
-            <img src="/verge.png" alt="Verge Logo" className="h-16 w-auto" />
-        </div>
-        <div className="text-xl pr-4 pt-2">
-            {publicKey ? <WalletDisconnectButton /> : <WalletMultiButton /> }
-        </div>
+        <div className="flex items-center gap-4">
+        <WagmiProvider config={config}>
+            <QueryClientProvider client={queryClient}>
+                <RainbowKitProvider>
+                    <ConnectButton />
+                </RainbowKitProvider>
+            </QueryClientProvider>
+    </WagmiProvider>
     </div>
+    </div>
+  );
 }
